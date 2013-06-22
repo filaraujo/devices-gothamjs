500813c33c9db6389303eecf679175c2aeeb91a8
Filipe Araujo <Filipe_Araujo@cable.comcast.com>
2013-06-22 12:46:26 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    routes = require('./routes'),
+    http = require('http'),
+    path = require('path'),
+    stylus = require('stylus'),
+    nib = require('nib');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(
+    stylus.middleware({
+        src: __dirname,
+        compile: function compile(str, path) {
+          return stylus(str)
+            .set('filename', path)
+            .set('compress', true)
+            .use(nib());
+        }
+    })
+);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+// development only
+if ('development' === app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
