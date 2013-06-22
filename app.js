231dadf00da37cac8f1c9a4da77e21410fa69760
Filipe Araujo <Filipe_Araujo@cable.comcast.com>
2013-06-22 19:18:32 -0400

    adding slides

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var express = require('express'),
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -23,7 +23,7 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(
     stylus.middleware({
-        src: __dirname,
+        src: __dirname + '/public',
         compile: function compile(str, path) {
           return stylus(str)
             .set('filename', path)
@@ -35,16 +35,12 @@ app.use(
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
-
-
 // development only
 if ('development' === app.get('env')) {
   app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
-
+var argv = require('optimist').argv;
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Express',
+        source: argv.source || false
+    });
 };
